5f031d8c0fc6008f94939c8122533ba0bfb39bc7
RohanKhatua <contact@rohankhatua.dev>
2024-09-07 13:26:58 +0530

    feat: switch to browserless

--- lib/scraper.ts
+++ lib/scraper.ts
@@ -1,17 +1,18 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 
 export async function scrapeAvailableSeats(url: string): Promise<string> {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.connect({
+        browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_TOKEN}`,
+    });
     const page = await browser.newPage();
 
     try {
         await page.goto(url, { waitUntil: "networkidle0" });
-        await page.waitForSelector('.slots button p:last-child')
+        await page.waitForSelector('.slots button p:last-child');
         const seatsText = await page.$eval('.slots button p:last-child', (el) => el.textContent);
         const availableSeats = seatsText?.match(/\d+/)?.[0] || "0";
         return availableSeats;
-    }
-    finally {
+    } finally {
         await browser.close();
     }
 }
